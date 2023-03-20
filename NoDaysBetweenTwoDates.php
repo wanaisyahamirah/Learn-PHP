cf782ad2c3a521559456738635ac5cb64de28bcd
Wan Aisyah Amirah <aisyahamirahzamri@gmail.com>
2023-03-21 07:32:18 +0800

    convert dates to unix tmstmp using strtotime()

--- NoDaysBetweenTwoDates.php
+++ NoDaysBetweenTwoDates.php
@@ -3,3 +3,7 @@
 // Define two variables
 $date1 = '2022-01-01';
 $date2 = '2022-01-31';
+
+// Convert two dates to Unix timestamps
+$timestamp1 = strtotime($date1);
+$timestamp2 = strtotime($date2);
