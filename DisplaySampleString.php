f98434eb03e0cdea2fb378086b2b80d08966dae2
Wan Aisyah Amirah <aisyahamirahzamri@gmail.com>
2023-03-22 23:29:19 +0800

    add code to display the strings

--- DisplaySampleString.php
+++ DisplaySampleString.php
@@ -4,5 +4,8 @@
 $string1 = "Tomorrow I 'll learn PHP global variables.";
 $string2 = "This is a bad command : del c:\\*.*";
 
+// Display the strings
+echo $string1 . "\n";
+echo $string2 . "\n";
 
 ?>
