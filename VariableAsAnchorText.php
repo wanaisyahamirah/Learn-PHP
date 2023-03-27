53f4027334e847b2b727494026a46dfd731e9627
Wan Aisyah Amirah <aisyahamirahzamri@gmail.com>
2023-03-28 07:40:57 +0800

    put variable into title, h3 tag as an anchor text

--- VariableAsAnchorText.php
+++ VariableAsAnchorText.php
@@ -1,3 +1,15 @@
 <?php
     $var = 'PHP Tutorial';
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php echo $var ?></title>
+</head>
+<body>
+	<h3><?php echo $var ?></h3>
+	<p>PHP, an acronym for Hypertext Preprocessor, is a widely-used open source general-purpose scripting language. It is a cross-platform, HTML embedded server-side scripting language and is especially suited for web development.</p>
+	<p>Go to the <a href="#"><?php echo $var ?></a>.</p>
+</body>
+</html>
