29c0953747610808c0130352e6be9caf4a114a2d
Wan Aisyah Amirah <aisyahamirahzamri@gmail.com>
2023-03-29 07:27:46 +0800

    get client IP address

--- GetClientIPAddress.php
+++ GetClientIPAddress.php
@@ -1,2 +1,12 @@
 <?php
+
+// Get client IP address
+if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+    $ip_address = $_SERVER['HTTP_CLIENT_IP'];
+} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $ip_address = $_SERVER['HTTP_X_FORWARDED_FOR'];
+} else {
+    $ip_address = $_SERVER['REMOTE_ADDR'];
+}
+
 ?>
