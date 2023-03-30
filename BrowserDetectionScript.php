dbf79c6b831270ae66fe7b7cda576ef5545c5628
Wan Aisyah Amirah <aisyahamirahzamri@gmail.com>
2023-03-30 23:17:48 +0800

    add code to define array of known browsers

--- BrowserDetectionScript.php
+++ BrowserDetectionScript.php
@@ -3,5 +3,16 @@
 // Get user agent string
 $user_agent = $_SERVER['HTTP_USER_AGENT'];
 
+// Define array of known browsers and their user agent strings
+$known_browsers = array(
+    'Chrome' => 'Chrome',
+    'Firefox' => 'Firefox',
+    'Safari' => 'Safari',
+    'Opera' => 'Opera',
+    'Internet Explorer' => 'MSIE',
+    'Edge' => 'Edge'
+  );
+  
+
 
 ?>
